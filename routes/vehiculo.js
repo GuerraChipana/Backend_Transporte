ef30aae650b906d2c4d6f75611d1a30b539725c2
Luis2104 <fernandoguerrachipana@gmail.com>
2024-10-03 08:14:49 -0500

    Avance 3 03/10/2024

--- routes/vehiculo.js
+++ routes/vehiculo.js
@@ -2,82 +2,120 @@ const express = require('express');
 const router = express.Router();
 const db = require('../db');
 
-// Obtener todos los vehículos con detalles de seguro y asociación
+// Obtener datos de los vehículos
 router.get('/', (req, res) => {
-    const consulta = `
-                      SELECT v.ID, v.PLACAS_ACTUAL, v.N_TARJETA, v.N_MOTOR,
-       v.MARCA, v.COLOR, v.ANIO_DE_COMPRA, 
-       s.N_POLIZA AS SEGURO, a.nombre AS ASOCIACION
-FROM vehiculo v
-JOIN seguro_vehicular s ON v.SEGURO_ID = s.ID
-JOIN asociaciones a ON v.ASOCIACION_ID = a.id
-
-                    `;
+    const consulta = `CALL SP_V_DatosVehiculo()`;
 
     db.query(consulta, (err, results) => {
         if (err) {
-            console.error('Error al obtener los vehículos:', err);
-            return res.status(500).json({ error: 'Error al obtener los vehículos' });
+            console.error('Error al ejecutar la consulta:', err);
+            return res.status(500).json({ error: 'Error al obtener datos de los vehículos', details: err.message });
         }
-        res.json(results);
+        res.json({ message: 'Datos obtenidos con éxito', data: results[0] });
     });
 });
 
 // Agregar un nuevo vehículo
 router.post('/', (req, res) => {
-    const { PLACAS_ACTUAL, N_TARJETA, N_MOTOR, MARCA, COLOR, ANIO_DE_COMPRA, SEGURO_ID, ASOCIACION_ID } = req.body;
+    const { PLACA, N_TARJETA, N_MOTOR, MARCA,
+        COLOR, ANO_DE_COMPRA, SEGURO_ID, ASOCIACION_ID,
+        N_POLIZA, FECHA_VIGENCIA_DESDE, FECHA_VIGENCIA_HASTA,
+        PROPIETARIO_1_ID, PROPIETARIO_2_ID, ID_USUARIO } = req.body;
+
+    // Validar los campos obligatorios
+    if (!PLACA || !N_TARJETA || !N_MOTOR || !MARCA ||
+        !COLOR || !ANO_DE_COMPRA || !SEGURO_ID || !ASOCIACION_ID ||
+        !N_POLIZA || !FECHA_VIGENCIA_DESDE || !FECHA_VIGENCIA_HASTA ||
+        !PROPIETARIO_1_ID || !PROPIETARIO_2_ID || !ID_USUARIO) {
+        return res.status(400).json({ error: 'Todos los campos son requeridos' });
+    }
 
     const consulta = `
-        INSERT INTO vehiculo (PLACAS_ACTUAL, N_TARJETA, N_MOTOR, MARCA, COLOR, ANIO_DE_COMPRA, SEGURO_ID, ASOCIACION_ID) 
-        VALUES (?, ?, ?, ?, ?, ?, ?, ?)
+        CALL SP_V_AgregarVehiculo(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
     `;
 
-    const valores = [PLACAS_ACTUAL, N_TARJETA, N_MOTOR, MARCA, COLOR, ANIO_DE_COMPRA, SEGURO_ID, ASOCIACION_ID];
+    const valores = [
+        PLACA, N_TARJETA, N_MOTOR, MARCA, COLOR,
+        ANO_DE_COMPRA, SEGURO_ID, ASOCIACION_ID,
+        N_POLIZA, FECHA_VIGENCIA_DESDE, FECHA_VIGENCIA_HASTA,
+        PROPIETARIO_1_ID, PROPIETARIO_2_ID, ID_USUARIO
+    ];
 
     db.query(consulta, valores, (err, result) => {
         if (err) {
             console.error('Error al agregar el vehículo:', err);
-            return res.status(500).json({ error: 'Error al agregar el vehículo' });
+            return res.status(500).json({ error: 'Error al agregar el vehículo', details: err.message });
         }
-        res.status(201).json({ message: 'Vehículo agregado exitosamente', vehiculoId: result.insertId });
+        res.status(201).json({ message: 'Vehículo agregado exitosamente', vehiculoId: result[0][0].vehiculoId });
     });
 });
 
-
-// Modificar un vehículo existente
+// Modificar un vehiculo
 router.put('/:id', (req, res) => {
     const { id } = req.params;
-    const { PLACAS_ACTUAL, N_TARJETA, N_MOTOR, MARCA, COLOR, ANIO_DE_COMPRA, SEGURO_ID, ASOCIACION_ID } = req.body;
+    const { PLACA, N_TARJETA, N_MOTOR, MARCA, COLOR, ANO_DE_COMPRA,
+        SEGURO_ID, ASOCIACION_ID, ID_USUARIO_MODIFICACION,
+        PROPIETARIO_1_ID, PROPIETARIO_2_ID, N_POLIZA,
+        FECHA_VIGENCIA_DESDE, FECHA_VIGENCIA_HASTA } = req.body;
+
+    // Validar campos requeridos
+    if (!PLACA || !N_TARJETA || !N_MOTOR || !MARCA || !COLOR || !ANO_DE_COMPRA ||
+        !SEGURO_ID || !ASOCIACION_ID || !ID_USUARIO_MODIFICACION ||
+        !PROPIETARIO_1_ID || !PROPIETARIO_2_ID || !N_POLIZA ||
+        !FECHA_VIGENCIA_DESDE || !FECHA_VIGENCIA_HASTA) {
+        return res.status(400).json({ error: 'Todos los campos son requeridos' });
+    }
 
     const consulta = `
-            UPDATE vehiculo 
-            SET PLACAS_ACTUAL = ?, N_TARJETA = ?, N_MOTOR = ?, MARCA = ?, COLOR = ?, ANIO_DE_COMPRA = ?, SEGURO_ID = ?, ASOCIACION_ID = ?
-            WHERE ID = ?
-        `;
+        CALL SP_V_ModificarVehiculo(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
+    `;
 
-    const valores = [PLACAS_ACTUAL, N_TARJETA, N_MOTOR, MARCA, COLOR, ANIO_DE_COMPRA, SEGURO_ID, ASOCIACION_ID, id];
+    const valores = [
+        id,
+        PLACA, N_TARJETA, N_MOTOR, MARCA, COLOR, ANO_DE_COMPRA,
+        SEGURO_ID, ASOCIACION_ID, ID_USUARIO_MODIFICACION,
+        PROPIETARIO_1_ID, PROPIETARIO_2_ID, N_POLIZA,
+        FECHA_VIGENCIA_DESDE, FECHA_VIGENCIA_HASTA
+    ];
+
 
     db.query(consulta, valores, (err) => {
         if (err) {
-            console.error('Error al actualizar el vehículo:', err);
-            return res.status(500).json({ error: 'Error al actualizar el vehículo' });
+            console.error('Error al modificar el vehículo:', err);
+            return res.status(500).json({ error: 'Error al modificar el vehículo', details: err.message });
         }
-        res.json({ message: 'Vehículo actualizado exitosamente' });
+        res.json({ message: 'Vehículo modificado exitosamente' });
     });
 });
 
-// Eliminar un vehículo
-router.delete('/:id', (req, res) => {
-    const { id } = req.params;
+// Cambier estado del vehiculo
+router.patch('/:id/estado', (req, res) => {
+    const id = req.params.id;
+    const { estado, id_usuario_modificacion } = req.body;
 
-    const consulta = 'DELETE FROM vehiculo WHERE ID = ?';
+    // Validar los campos obligatorios
+    if (estado === undefined || !id_usuario_modificacion) {
+        return res.status(400).json({ error: 'Todos los campos son requeridos' });
+    }
 
-    db.query(consulta, [id], (err) => {
+    const consulta = `
+        CALL SP_V_EstadoVehiculo(?, ?, ?)
+    `;
+
+    const valores = [id, estado, id_usuario_modificacion];
+
+    db.query(consulta, valores, (err, result) => {
         if (err) {
-            console.error('Error al eliminar el vehículo:', err);
-            return res.status(500).json({ error: 'Error al eliminar el vehículo' });
+            console.error('Error al cambiar el estado del vehículo:', err);
+            return res.status(500).json({ error: 'Error al cambiar el estado', details: err.message });
         }
-        res.json({ message: 'Vehículo eliminado exitosamente' });
+
+        // Verificar si se encontró el vehículo y se realizó la modificación
+        if (result[0][0].affectedRows === 0) {
+            return res.status(404).json({ error: 'Vehículo no encontrado' });
+        }
+
+        res.json({ message: 'Estado del vehículo actualizado exitosamente' });
     });
 });
 
